5ed4b15108945162b27036a7d0ced51b76fe0e4a
Jamir Gutierrez <106436201+JamirGDC@users.noreply.github.com>
2024-02-17 14:44:07 +0000

    done

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, abort
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite, Planet, People
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,92 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    result = []
+    for person in people:
+        result.append({'id': person.id, 'name': person.name})
+    return jsonify(result)
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get_or_404(people_id)
+    return jsonify({'id': person.id, 'name': person.name})
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    result = []
+    for planet in planets:
+        result.append({'id': planet.id, 'name': planet.name})
+    return jsonify(result)
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    return jsonify({'id': planet.id, 'name': planet.name})
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    result = []
+    for user in users:
+        result.append({'id': user.id, 'username': user.username})
+    return jsonify(result)
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    user_id = 1
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+    result = []
+    for favorite in favorites:
+        if favorite.planet_id:
+            result.append({'type': 'planet', 'id': favorite.planet_id})
+        elif favorite.people_id:
+            result.append({'type': 'people', 'id': favorite.people_id})
+    return jsonify(result)
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user_id = 1
+    new_favorite = Favorite(user_id=user_id, planet_id=planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify({'message': 'Favorite planet added'})
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    user_id = 1 
+    new_favorite = Favorite(user_id=user_id, people_id=people_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify({'message': 'Favorite people added'})
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    user_id = 1
+    favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Favorite planet deleted'})
+    else:
+        return jsonify({'message': 'Favorite planet not found'})
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    user_id = 1
+    favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Favorite people deleted'})
+    else:
+        return jsonify({'message': 'Favorite people not found'})
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
